0f16bfbc0b209f805fc8fe37d9cad59188244dd0
jgeeliss <jgeeliss@gmail.com>
2020-05-20 21:06:51 +0200

    initial commit

---
+++ fetchJson.js
@@ -0,0 +1,23 @@
+const fetch = require("node-fetch");
+const fs = require("fs");
+
+try {
+    var url = fs.readFileSync('./url.conf', 'utf8');
+} catch(e) {
+    console.log('Error:', e.stack);
+}
+
+console.log(url);
+
+let settings = { method: "Get" };
+
+fetch(url, settings)
+    .then(res => res.json())
+    .then((jsonObject) => {
+		for (var i = 0, len = Object.keys(jsonObject.results).length; i < len; i++) {
+			console.log(jsonObject.results[i].href + " of type " + jsonObject.results[i].type);	
+		}	
+	
+    })
+	;
+  
