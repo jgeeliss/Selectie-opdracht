babb412cfcb63369341b0054566b0cfb27ea38c4
jgeeliss <jgeeliss@gmail.com>
2020-05-21 20:35:50 +0200

    fetching relationships

--- fetchJson.js
+++ fetchJson.js
@@ -9,48 +9,61 @@ try {
 
 let settings = { method: "Get" };
 
-//let nextResultsUrl= url + "/sam/organisationalunits?limit=5000"
-let nextResultsUrl= url + "/sam/organisationalunits?limit=5000&keyOffset=2018-10-06T21%3A22%3A36.947Z,93b5c620-6f84-4c84-b78e-233a07f0732c"
 
-async function getJson() {
-	const response = await fetch(nextResultsUrl, settings);
+
+async function getJson(inputUrl) {
+	const response = await fetch(inputUrl, settings);
 	jsonObject = await response.json();
 	return jsonObject;
 }
 
-let jsonObjectBig = [];
-var nexturl = "";
+let organUnits = [];
+let relationships = [];
+
 
 const fetchAllJson = async _ => {
 	console.log('Start')
 
+	//let nextResultsUrl= url + "/sam/organisationalunits?limit=5000"
+	let nextResultsUrl= url + "/sam/organisationalunits?limit=5000&keyOffset=2018-10-06T21%3A22%3A36.947Z,93b5c620-6f84-4c84-b78e-233a07f0732c"
+	var nexturl = "";
+
 	while (nexturl != undefined) {
-	//for (var i = 0, len = 4; i < len; i++) {
-		const jsonObject = await getJson();
-		console.log(jsonObject.results[1].href + " by name " + jsonObject.results[1].$$expanded.$$displayName);
-		//jsonObjectBig.push(jsonObject);
-		//jsonObjectBig = jsonObjectBig.concat(jsonObject);
-		jsonObjectBig = jsonObjectBig.concat(jsonObject);
-		nexturl = jsonObject.$$meta.next;		 
+		const jsonUnitObject = await getJson(nextResultsUrl);
+		console.log(jsonUnitObject.results[1].href + " by name " + jsonUnitObject.results[1].$$expanded.$$displayName);
+		
+		for (var i = 0, len = Object.keys(jsonUnitObject.results).length; i < len; i++) {
+			const OU = new Object();
+			OU.href = jsonUnitObject.results[i].href;
+			OU.type = jsonUnitObject.results[i].$$expanded.type;
+			OU.name = jsonUnitObject.results[i].$$expanded.$$displayName;
+			OU.parents = [];
+			organUnits.push(OU);			
+		}		
+		nexturl = jsonUnitObject.$$meta.next;		 
 		nextResultsUrl =  url + nexturl;
-		//console.log(nexturl);
-		console.log(jsonObjectBig.length);
+		console.log(organUnits.length);
 	}
 
-	//console.log(jsonObjectBig[jsonObjectBig.length-1].results[0].$$expanded.$$displayName)
+	nextResultsUrl= url + "/sam/organisationalunits/relations?type=IS_PART_OF&limit=5000&keyOffset=2018-10-06T21%3A34%3A07.484Z,1c5ecc04-7045-4863-9129-8a6f323564f4"
+	nexturl = "";
 
-	jsonObjectBig.forEach(
-		function checkIfPartOf(item) {
-			for (var i = 0, len = Object.keys(item.results).length; i < len; i++) {
-				//check if it's part of another OU
-				//const 
-				// example=https://api.katholiekonderwijs.vlaanderen/sam/organisationalunits/relations?type=IS_MEMBER_OF&from=/sam/organisationalunits/c000eaea-ab47-2590-e044-d4856467bfb8
-				if (item.results[i].$$expanded.$$meta.type == "ORGANISATIONAL_UNIT") {
-					console.log(item.results[i].$$expanded.$$displayName)
-				}
+	while (nexturl != undefined) {
+			const jsonRelationObject = await getJson(nextResultsUrl);
+			console.log(jsonRelationObject.results[1].href + " by name " + jsonRelationObject.results[1].$$expanded.$$displayName);
+			console.log(jsonRelationObject.$$meta.next);
+
+			for (var i = 0, len = Object.keys(jsonRelationObject.results).length; i < len; i++) {
+				const rel = new Object();
+				rel.from = jsonRelationObject.results[i].href;
+				rel.to = jsonRelationObject.results[i].$$expanded.type;	
+				relationships.push(rel);				
 			}
-		}
-	)
+			
+			nexturl = jsonRelationObject.$$meta.next;		 
+			nextResultsUrl =  url + nexturl;	
+			console.log(relationships.length);
+	}
 
 	console.log("End")	
   }
