d9600a91278042e174ba4b9939fa5ade98d5756d
jgeeliss <jgeeliss@gmail.com>
2020-05-21 14:55:13 +0200

    added json array

--- fetchJson.js
+++ fetchJson.js
@@ -7,18 +7,54 @@ try {
     console.log('Error:', e.stack);
 }
 
-console.log(url);
-
 let settings = { method: "Get" };
 
-fetch(url, settings)
-    .then(res => res.json())
-    .then((jsonObject) => {
-		for (var i = 0, len = Object.keys(jsonObject.results).length; i < len; i++) {
-			console.log(jsonObject.results[i].href + " of type " + jsonObject.results[i].type);	
-		}	
-	
-    })
-	;
-  
-    //added comment
+//let nextResultsUrl= url + "/sam/organisationalunits?limit=5000"
+let nextResultsUrl= url + "/sam/organisationalunits?limit=5000&keyOffset=2018-10-06T21%3A22%3A36.947Z,93b5c620-6f84-4c84-b78e-233a07f0732c"
+
+async function getJson() {
+	const response = await fetch(nextResultsUrl, settings);
+	jsonObject = await response.json();
+	return jsonObject;
+}
+
+let jsonObjectBig = [];
+var nexturl = "";
+
+const fetchAllJson = async _ => {
+	console.log('Start')
+
+	while (nexturl != undefined) {
+	//for (var i = 0, len = 4; i < len; i++) {
+		const jsonObject = await getJson();
+		console.log(jsonObject.results[1].href + " by name " + jsonObject.results[1].$$expanded.$$displayName);
+		//jsonObjectBig.push(jsonObject);
+		//jsonObjectBig = jsonObjectBig.concat(jsonObject);
+		jsonObjectBig = jsonObjectBig.concat(jsonObject);
+		nexturl = jsonObject.$$meta.next;		 
+		nextResultsUrl =  url + nexturl;
+		//console.log(nexturl);
+		console.log(jsonObjectBig.length);
+	}
+
+	//console.log(jsonObjectBig[jsonObjectBig.length-1].results[0].$$expanded.$$displayName)
+
+	jsonObjectBig.forEach(
+		function checkIfPartOf(item) {
+			for (var i = 0, len = Object.keys(item.results).length; i < len; i++) {
+				//check if it's part of another OU
+				//const 
+				// example=https://api.katholiekonderwijs.vlaanderen/sam/organisationalunits/relations?type=IS_MEMBER_OF&from=/sam/organisationalunits/c000eaea-ab47-2590-e044-d4856467bfb8
+				if (item.results[i].$$expanded.$$meta.type == "ORGANISATIONAL_UNIT") {
+					console.log(item.results[i].$$expanded.$$displayName)
+				}
+			}
+		}
+	)
+
+	console.log("End")	
+  }
+
+
+
+fetchAllJson();
