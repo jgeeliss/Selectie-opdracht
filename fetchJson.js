ee0f6bf169eaf95025080e767b16a343a47e839c
jgeeliss <jgeeliss@gmail.com>
2020-05-22 22:37:19 +0200

    add top OUs

--- fetchJson.js
+++ fetchJson.js
@@ -19,20 +19,21 @@ async function getJson(inputUrl) {
 
 let organUnits = [];
 let relationships = [];
+let topOrganUnits = [];
 
 
 const fetchAllJson = async _ => {
 	console.log('Start')
 
-	//let nextResultsUrl= url + "/sam/organisationalunits?limit=5000"
-	let nextResultsUrl= url + "/sam/organisationalunits?limit=5000&keyOffset=2018-10-06T21%3A22%3A36.947Z,93b5c620-6f84-4c84-b78e-233a07f0732c"
+	let nextResultsUrl= url + "/sam/organisationalunits?limit=5000"
+	//let nextResultsUrl= url + "/sam/organisationalunits?limit=5000&keyOffset=2018-10-06T21%3A22%3A36.947Z,93b5c620-6f84-4c84-b78e-233a07f0732c"
 	var nexturl = "";
 
 	while (nexturl != undefined) {
 		const jsonUnitObject = await getJson(nextResultsUrl);
-		console.log(jsonUnitObject.results[1].href + " by name " + jsonUnitObject.results[1].$$expanded.$$displayName);
+		console.log(jsonUnitObject.results[0].href + " by name " + jsonUnitObject.results[0].$$expanded.$$displayName);
 		
-		for (var i = 0, len = Object.keys(jsonUnitObject.results).length; i < len; i++) {
+		for (var i = 0; i < Object.keys(jsonUnitObject.results).length; i++) {
 			const OU = new Object();
 			OU.href = jsonUnitObject.results[i].href;
 			OU.type = jsonUnitObject.results[i].$$expanded.type;
@@ -44,14 +45,15 @@ const fetchAllJson = async _ => {
 		nextResultsUrl =  url + nexturl;
 	}
 
-	nextResultsUrl= url + "/sam/organisationalunits/relations?type=IS_PART_OF&limit=5000&keyOffset=2018-10-06T21%3A34%3A07.484Z,1c5ecc04-7045-4863-9129-8a6f323564f4"
+	nextResultsUrl= url + "/sam/organisationalunits/relations?type=IS_PART_OF&limit=5000"
+	//nextResultsUrl= url + "/sam/organisationalunits/relations?type=IS_PART_OF&limit=5000&keyOffset=2018-10-06T21%3A34%3A07.484Z,1c5ecc04-7045-4863-9129-8a6f323564f4"
 	nexturl = "";
 
 	while (nexturl != undefined) {
 			const jsonRelationObject = await getJson(nextResultsUrl);
-			console.log(jsonRelationObject.results[1].$$expanded.from.href + " -> " + jsonRelationObject.results[1].$$expanded.to.href);
+			console.log(jsonRelationObject.results[0].$$expanded.from.href + " -> " + jsonRelationObject.results[0].$$expanded.to.href);
 			
-			for (var i = 0, len = Object.keys(jsonRelationObject.results).length; i < len; i++) {
+			for (var i = 0; i < Object.keys(jsonRelationObject.results).length; i++) {
 				const rel = new Object();
 				rel.from = jsonRelationObject.results[i].$$expanded.from.href;
 				rel.to = jsonRelationObject.results[i].$$expanded.to.href;	
@@ -61,32 +63,78 @@ const fetchAllJson = async _ => {
 			nexturl = jsonRelationObject.$$meta.next;		 
 			nextResultsUrl =  url + nexturl;	
 	}
-
-	//USE RELATIONSHIPS TO FILL IN PARENTS
 	console.log(organUnits.length);
 	console.log(relationships.length);
-	organUnits.forEach(
-		function lookupParts(unit) {
-			relationships.forEach(
-				function loopOverRelationships(relation) {
-					if (unit.href == relation.from) {
-						unit.parents.push(relation.to);	
-					}
-				}
-			)
+
+
+	//FIND TOP OU's
+	topNodeFound=0;
+	let isTop = true; //node is top until proven otherwise
+	check=0;
+
+	nonTopOU = [  'BOARDING',	'SCHOOL',	'SCHOOLENTITY',	'CLASS',	'TEACHER_GROUP',	'SCHOOLENTITY_CLUSTER']
+
+	for (var i = 0; i < organUnits.length; i++){
+		//if (nonTopOU.includes(organUnits[i].type)) {break;}	
+		for (var y = 0; y < relationships.length; y++){
+			if (organUnits[i].href == relationships[y].from) {
+				isTop = false;
+				break;}	
+		}	
+		check++;	
+		if (check%1000==0){console.log("checked " + check/1000 + " thousand OUs");}
+		if (isTop==true) {
+			topOrganUnits.push(organUnits[i]);
+			topNodeFound++;
+			console.log("found " + topNodeFound + " top parents");
+			isTop = true;
 		}
-	)
-
-	//CHECK IF PARENTS HAVE BEEN FOUND
-	for (var i = 0; i < 10000; i++) {
-		if (organUnits.parents != undefined) {		
-			 console.log(organUnits[i]);
-		 }
+		//if (topNodeFound%1000==0) {console.log("checked " + topNodeFound/1000 + " billion parents");}	
 	}
+	console.log("number of tops = " + topOrganUnits.length);
+	
+	for (var i = 0; i < 10; i++) {console.log(topOrganUnits[i]);}
 
-	console.log("End")	
+	//find types of top OU
+	allOUtypes=[]		
+	for (var i = 0; i < topOrganUnits.length; i++){
+		allOUtypes.push(topOrganUnits[i].type)
+	}
+	let uniqueOUtypes = [...new Set(allOUtypes)];
+	console.log(uniqueOUtypes)
+
+	/*
+	//USE RELATIONSHIPS TO FILL IN PARENTS
+	numberOfParentsAdded = 0;
+	
+	for (var i = 0; i < organUnits.length; i++){
+		for (var y = 0; y < relationships.length; y++){
+			if (organUnits[i].href == relationships[y].from) {
+				organUnits[i].parents.push(relationships[y].to);
+				numberOfParentsAdded++;	
+			}		
+			check++;	
+			if (check%1000000000==0){console.log("checked " + check/1000000000 + " billion parents");}		
+		}	
+	}
+	console.log("Added " + numberOfParentsAdded + " parents");
+	
+
+	//CHECK IF PARENTS HAVE BEEN ADDED
+	numberOfParentsFound = 0;
+	for (var i = 0; i < organUnits.length; i++) {
+		if (organUnits[i].parents.length >0) {		
+			numberOfParentsFound++;				
+		}
+	}
+	console.log("Found " + numberOfParentsFound + " parents");
+	*/
+
+	console.log("End");	
+	var end = new Date() - start
+	console.log('Execution time: %dms', end)
   }
 
 
-
+var start = new Date()
 fetchAllJson();
